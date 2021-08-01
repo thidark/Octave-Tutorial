2c9f23349707b5ad17c8d862fec47717af3364d5
Thidar <thidarkhaing078@gmail.com>
2021-08-01 22:33:42 +0630

    Octave Basic Operations

---
+++ Basic_Operation.m
@@ -0,0 +1,65 @@
+5+6
+5-3
+4*8
+5/2
+5%2
+2^6
+1==2
+1!=2
+1 && 0 %AND
+1 || 0 %OR
+xor(1,0)
+PS1('>> ');
+a=3
+a
+a=3; % semicolon supressing output
+a=3
+b='hi'
+b
+a=pi;
+a
+disp(a);
+disp(sprintf('2 decimal: %0.2f',a))
+disp(sprintf('6 decimal: %0.6f',a))
+format long
+a
+format short
+a
+
+A=[1 2;3 4; 5 6]
+
+A=ones(2,2);
+
+A=zeros(3,3);
+
+B=2*ones(2,2);
+
+v=1:0.1:2
+
+v=1:6
+
+w=rand(1,3)
+
+rand(3,3)
+
+w=randn(1,3)
+
+w=randn(3,3)
+
+w=-6+sqrt(10)*(randn(1,10000))
+
+hist(w)
+
+hist(w,50)
+
+eye(4)
+
+I=eye(5)
+
+help eye
+
+help rand
+
+help help
+
+
